04006589235f2d71a26dd6b768dbde669a686332
korovindenis <korovindenis@yandex.ru>
2023-08-02 12:37:25 +0300

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+
+	storage "github.com/korovindenis/go-pc-metrics/internal/adapter/storage/memory"
+	usecaseServer "github.com/korovindenis/go-pc-metrics/internal/domain/usecase/server"
+	"github.com/korovindenis/go-pc-metrics/internal/http/handler"
+	"github.com/korovindenis/go-pc-metrics/internal/http/server"
+)
+
+const http_address = "0.0.0.0:8080"
+
+func main() {
+	// init bd
+	storage, err := storage.New()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// init server usecases
+	srvUscs, err := usecaseServer.New(storage)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// init server handlers
+	srvHdlrs, err := handler.New(srvUscs)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// run web server
+	if err := server.Exec(http_address, srvHdlrs); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
