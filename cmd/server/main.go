71ac2ccbf782be07443f8ad89d6bced5463d1fcc
korovindenis <korovindenis@yandex.ru>
2023-09-06 13:51:07 +0300

    iter9

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/korovindenis/go-pc-metrics/internal/adapters/config"
-	storage "github.com/korovindenis/go-pc-metrics/internal/adapters/storage/memory"
+	storage "github.com/korovindenis/go-pc-metrics/internal/adapters/storage/disk"
 	serverusecase "github.com/korovindenis/go-pc-metrics/internal/domain/usecases/server"
 	"github.com/korovindenis/go-pc-metrics/internal/logger"
 	serverhandler "github.com/korovindenis/go-pc-metrics/internal/server/handler"
