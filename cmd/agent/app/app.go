0f743124a7f8340a3efdca1074c2ad9e28f98a3b
Denis Korovin <korovindenis@yandex.ru>
2023-09-12 16:19:27 +0300

    iter12

--- cmd/agent/app/app.go
+++ cmd/agent/app/app.go
@@ -103,107 +103,46 @@ func sendMetrics(restClient *http.Client, metricsVal any, log logger, httpServer
 	default:
 		return errors.New("sendMetrics(): metricsVal not recognized")
 	}
-	if err := httpReq(restClient, log, httpServerAddress, metrics, 1000000); err != nil {
+	if err := httpReq(restClient, log, httpServerAddress, metrics); err != nil {
 		return fmt.Errorf("sendMetrics entity.CounterType: %s", err)
 	}
 	return nil
 }
 
 // send data
-func httpReq(restClient *http.Client, log logger, httpServerAddress string, metrics []entity.Metrics, batchSize int) error {
-	// Проверяем, что размер батча больше нуля
-	if batchSize <= 0 {
-		return fmt.Errorf("batch size must be greater than zero")
+func httpReq(restClient *http.Client, log logger, httpServerAddress string, metrics []entity.Metrics) error {
+	var compressedBody bytes.Buffer
+	gz := gzip.NewWriter(&compressedBody)
+
+	jsonBody, err := json.Marshal(metrics)
+	if err != nil {
+		return fmt.Errorf("err in Marshal: %s", err)
 	}
 
-	// Создаем срез для хранения батчей
-	batches := make([][]entity.Metrics, 0)
+	log.Info("send: " + string(jsonBody))
 
-	// Разбиваем данные на батчи
-	for i := 0; i < len(metrics); i += batchSize {
-		end := i + batchSize
-		if end > len(metrics) {
-			end = len(metrics)
-		}
-		batch := metrics[i:end]
-		batches = append(batches, batch)
+	_, err = gz.Write(jsonBody)
+	if err != nil {
+		return fmt.Errorf("err in gz Write: %s", err)
+	}
+	gz.Close()
+
+	req, err := http.NewRequest("POST", httpServerAddress+"/updates/", &compressedBody)
+	if err != nil {
+		return fmt.Errorf("err in NewRequest: %s", err)
 	}
 
-	// Отправляем каждый батч
-	for _, batch := range batches {
-		// Сначала сжимаем данные в формат Gzip
-		var compressedBody bytes.Buffer
-		gz := gzip.NewWriter(&compressedBody)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Encoding", "gzip")
+	req.Header.Set("Accept-Encoding", "gzip")
 
-		jsonBody, err := json.Marshal(batch)
-		if err != nil {
-			return fmt.Errorf("err in Marshal: %s", err)
-		}
-
-		_, err = gz.Write(jsonBody)
-		if err != nil {
-			return fmt.Errorf("err in gz Write: %s", err)
-		}
-
-		gz.Close()
-
-		// Создаем запрос с сжатыми данными и устанавливаем заголовки
-		req, err := http.NewRequest("POST", httpServerAddress+"/updates/", &compressedBody)
-		if err != nil {
-			return fmt.Errorf("err in NewRequest: %s", err)
-		}
-
-		// Устанавливаем заголовки для сжатия и JSON
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Content-Encoding", "gzip")
-
-		// Отправляем запрос
-		resp, err := restClient.Do(req)
-		if err != nil {
-			log.Info(fmt.Sprintf("err in httpclient: %s", err))
-		} else {
-			defer resp.Body.Close()
-			log.Info("Status Code:" + resp.Status)
-		}
+	resp, err := restClient.Do(req)
+	if err != nil {
+		log.Info(fmt.Sprintf("err in httpclient: %s", err))
+	} else {
+		defer resp.Body.Close()
+		log.Info("Status Code:" + resp.Status)
 	}
 
 	return nil
 }
-
-// send data
-// func httpReq(restClient *http.Client, log logger, httpServerAddress string, metrics []entity.Metrics) error {
-// 	var compressedBody bytes.Buffer
-// 	gz := gzip.NewWriter(&compressedBody)
-
-// 	jsonBody, err := json.Marshal(metrics)
-// 	if err != nil {
-// 		return fmt.Errorf("err in Marshal: %s", err)
-// 	}
-
-// 	log.Info("send: " + string(jsonBody))
-
-// 	_, err = gz.Write(jsonBody)
-// 	if err != nil {
-// 		return fmt.Errorf("err in gz Write: %s", err)
-// 	}
-// 	gz.Close()
-
-// 	req, err := http.NewRequest("POST", httpServerAddress+"/updates/", &compressedBody)
-// 	if err != nil {
-// 		return fmt.Errorf("err in NewRequest: %s", err)
-// 	}
-
-// 	req.Header.Set("Content-Type", "application/json")
-// 	req.Header.Set("Content-Encoding", "gzip")
-// 	req.Header.Set("Accept-Encoding", "gzip")
-
-// 	resp, err := restClient.Do(req)
-// 	if err != nil {
-// 		log.Info(fmt.Sprintf("err in httpclient: %s", err))
-// 	} else {
-// 		defer resp.Body.Close()
-// 		log.Info("Status Code:" + resp.Status)
-// 	}
-
-// 	return nil
-// }
