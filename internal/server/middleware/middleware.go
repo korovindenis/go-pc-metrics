9dcbcc3192ead2cc0117bae1ff66daefbb220440
Denis Korovin <korovindenis@yandex.ru>
2023-09-13 14:52:04 +0300

    iter12

--- internal/server/middleware/middleware.go
+++ internal/server/middleware/middleware.go
@@ -25,55 +25,54 @@ func CheckMethod() gin.HandlerFunc {
 }
 
 func GzipMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if strings.Contains(c.GetHeader("Content-Encoding"), "gzip") {
-			// reader, err := gzip.NewReader(c.Request.Body)
-			// if err != nil {
-			// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Gzip data"})
-			// 	c.Abort()
-			// 	return
-			// }
-			// defer reader.Close()
-			//
-
-			var requestBody []byte
-			var maxMemory int64 = 64 << 20 // 64 MB
-			reader, _ := gzip.NewReader(&io.LimitedReader{R: c.Request.Body, N: maxMemory})
-			requestBody, _ = ioutil.ReadAll(reader)
-			c.Request.Body.Close()
-			bf := bytes.NewBuffer(requestBody)
-			c.Request.Body = http.MaxBytesReader(c.Writer, ioutil.NopCloser(bf), maxMemory)
-			//
-			//c.Request.Body = http.MaxBytesReader(c.Writer, reader, c.Request.ContentLength)
-		}
-		c.Next()
-	}
-	// var maxMemory int64 = 64 << 20 // 64 MB
-
 	// return func(c *gin.Context) {
-	// 	var requestBody []byte
-	// 	isGzip := false
-	// 	safe := &io.LimitedReader{R: c.Request.Body, N: maxMemory}
+	// 	if strings.Contains(c.GetHeader("Content-Encoding"), "gzip") {
+	// 		// reader, err := gzip.NewReader(c.Request.Body)
+	// 		// if err != nil {
+	// 		// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Gzip data"})
+	// 		// 	c.Abort()
+	// 		// 	return
+	// 		// }
+	// 		// defer reader.Close()
+	// 		//
 
-	// 	if c.GetHeader("Content-Encoding") == "gzip" {
-	// 		reader, err := gzip.NewReader(safe)
-	// 		if err == nil {
-	// 			isGzip = true
-	// 			requestBody, _ = ioutil.ReadAll(reader)
-	// 		}
+	// 		var requestBody []byte
+	// 		var maxMemory int64 = 64 << 20 // 64 MB
+	// 		reader, _ := gzip.NewReader(&io.LimitedReader{R: c.Request.Body, N: maxMemory})
+	// 		requestBody, _ = ioutil.ReadAll(reader)
+	// 		c.Request.Body.Close()
+	// 		bf := bytes.NewBuffer(requestBody)
+	// 		c.Request.Body = http.MaxBytesReader(c.Writer, ioutil.NopCloser(bf), maxMemory)
+	// 		//
+	// 		//c.Request.Body = http.MaxBytesReader(c.Writer, reader, c.Request.ContentLength)
 	// 	}
-
-	// 	if !isGzip {
-	// 		requestBody, _ = ioutil.ReadAll(safe)
-	// 	}
-
-	// 	c.Request.Body.Close()
-	// 	bf := bytes.NewBuffer(requestBody)
-	// 	c.Request.Body = http.MaxBytesReader(c.Writer, ioutil.NopCloser(bf), maxMemory)
-	// 	c.Set(ginx.ReqBodyKey, requestBody)
-
 	// 	c.Next()
 	// }
+	var maxMemory int64 = 64 << 20 // 64 MB
+
+	return func(c *gin.Context) {
+		var requestBody []byte
+		isGzip := false
+		safe := &io.LimitedReader{R: c.Request.Body, N: maxMemory}
+
+		if c.GetHeader("Content-Encoding") == "gzip" {
+			reader, err := gzip.NewReader(safe)
+			if err == nil {
+				isGzip = true
+				requestBody, _ = ioutil.ReadAll(reader)
+			}
+		}
+
+		if !isGzip {
+			requestBody, _ = ioutil.ReadAll(safe)
+		}
+
+		c.Request.Body.Close()
+		bf := bytes.NewBuffer(requestBody)
+		c.Request.Body = http.MaxBytesReader(c.Writer, ioutil.NopCloser(bf), maxMemory)
+
+		c.Next()
+	}
 }
 
 type gzipResponseWriter struct {
