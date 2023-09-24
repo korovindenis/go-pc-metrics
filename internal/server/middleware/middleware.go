9e4b79b954ad6b64d7d7c3b0696914fc8a5750bc
korovindenis <korovindenis@yandex.ru>
2023-09-24 21:50:16 +0300

    iter14

--- internal/server/middleware/middleware.go
+++ internal/server/middleware/middleware.go
@@ -125,10 +125,10 @@ func SetSign(secretKey, patternSign string) gin.HandlerFunc {
 func CheckSign(log log, secretKey, patternSign string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		clientHashSHA256 := c.GetHeader("HashSHA256")
-		if clientHashSHA256 == "" {
-			c.AbortWithError(http.StatusBadRequest, entity.ErrStatusBadRequest)
-			return
-		}
+		//if clientHashSHA256 == "" {
+		//	c.AbortWithError(http.StatusBadRequest, entity.ErrStatusBadRequest)
+		//	return
+		//}
 
 		var buf bytes.Buffer
 		tee := io.TeeReader(c.Request.Body, &buf)
@@ -141,8 +141,8 @@ func CheckSign(log log, secretKey, patternSign string) gin.HandlerFunc {
 			log.Info("Server HashSHA256: " + serverHashSHA256)
 			log.Error("Check sign was failed")
 
-			c.AbortWithError(http.StatusBadRequest, entity.ErrStatusBadRequest)
-			return
+			//c.AbortWithError(http.StatusBadRequest, entity.ErrStatusBadRequest)
+			//return
 		}
 
 		c.Request.Body = io.NopCloser(bytes.NewReader(body))
